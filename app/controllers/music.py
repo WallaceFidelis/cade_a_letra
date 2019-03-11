61d4f2694c48266e9919a535e0177d86d74d4c7f
WFidelis <wallacefid@gmail.com>
2019-03-11 20:27:35 -0300

    refactor: route /music

--- app/controllers/music.py
+++ app/controllers/music.py
@@ -1,28 +1,37 @@
 import requests
 import json 
 from app import app
-from app import db
 from app.controllers.spotify import *
-from flask import redirect, request, session
+from app.controllers.vagalume import *
+from app.models.tables import Music
+from flask import redirect, request, session, render_template
 from flask_session.__init__ import Session
-
 
 @app.route("/music")
 def get_music():
     if "authorization" not in session:
         return redirect("/")
 
-    artist, music = get_currently_playing()
-    if (artist == None and music == None):
-        artist, music = get_most_recently_playing()
+    searched_artist, searched_music = get_currently_playing()
+    if (searched_artist == None and searched_music == None):
+        searched_artist, searched_music = get_most_recently_playing()
 
-    data = {
-        "artist": artist,
-        "music": music
-    }
-    response = app.response_class(
-        response=json.dumps(data),
-        status=200,
-        mimetype='application/json'
-    )
-    return  response
+    music, artist, song_lyrics = search_music(searched_artist, searched_music)
+    song_lyrics = song_lyrics.split("\n")
+    return render_template('music.html', 
+                        music=music, 
+                        artist=artist, 
+                        searched_artist=searched_artist, 
+                        searched_music=searched_music, 
+                        song_lyrics=song_lyrics)
+
+
+def search_music(searched_artist, searched_music):
+
+    music, artist, song_lyrics = Music.get_Music(searched_artist, searched_music)
+    if(music == None):
+        music, artist, song_lyrics = get_music_vagalume(searched_artist, searched_music)
+        if music :
+            Music(artist, music,song_lyrics, searched_artist, searched_music).insert_music()    
+
+    return music, artist, song_lyrics
